c0520df5c6cd218a8f3f368dda034c74c294d1e9
duchnv-vn <huynhnguyenvietduc@gmail.com>
2024-02-06 17:04:14 +0700

    Section 8: UseRef in React Native

--- App.tsx
+++ App.tsx
@@ -1,5 +1,12 @@
-import React from 'react';
-import {SafeAreaView, Text, View, useColorScheme} from 'react-native';
+import React, {useRef} from 'react';
+import {
+  Button,
+  SafeAreaView,
+  ScrollView,
+  Text,
+  View,
+  useColorScheme,
+} from 'react-native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 import Header from './components/layout/Header/Header';
@@ -11,12 +18,27 @@ function App(): React.JSX.Element {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  const scrollViewRef = useRef(null);
+
+  const array = Array(200).fill(0);
+
+  const handleScrollTop = () => {
+    scrollViewRef.current.scrollTo({x: 0, y: 0});
+  };
+
   return (
     <SafeAreaView style={backgroundStyle}>
       <Header title="App Header" />
-      <View>
-        <Text>Main contents container</Text>
-      </View>
+      <ScrollView ref={scrollViewRef}>
+        {array.map((item, index) => {
+          return (
+            <Text onPress={() => alert(`The text ${index} has been pressed!`)}>
+              Main contents container {index}
+            </Text>
+          );
+        })}
+        <Button onPress={handleScrollTop} title="TOP" />
+      </ScrollView>
       <View>
         <Text>Footer contents container</Text>
       </View>
